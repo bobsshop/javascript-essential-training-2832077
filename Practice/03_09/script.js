cb95ac245a4797aaa17a0ca9b556c410d58a5b11
bobsshop <93291119+bobsshop@users.noreply.github.com>
2022-08-26 17:27:48 -0400

    ceated new JS method

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -23,4 +23,7 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  newPocketNum: function (numPockets) {
+    this.pocketNum = numPockets;
+  }
 };
